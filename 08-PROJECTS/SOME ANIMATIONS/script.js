47ed29098f8762db41ef9379356f84f6be08135a
bxluwatif3 <sammiemarvie03@gmail.com>
2026-08-13 03:23:38 +0100

    simple css animation

--- 08-PROJECTS/SOME ANIMATIONS/script.js
+++ 08-PROJECTS/SOME ANIMATIONS/script.js
@@ -7,6 +7,6 @@ btn.addEventListener('click', () => {
     // box.classList.add('move'); 
     // this adds a css class to the box element when the button is clicked, triggering the animation defined in the CSS.
     
-    // to increase the functionality, we use toggle method to add and remove the class on each click, allowing the animation to be triggered multiple times.
+    // to optimize the functionality, we use toggle method to add and remove the class on each click, allowing the animation to be triggered multiple times.
     box.classList.toggle('move');
 })
