d99d706e32b0cdaa7d659726fe29f8f32f56530c
bxluwatif3 <sammiemarvie03@gmail.com>
2026-08-21 14:28:10 +0100

    ripple

--- 08-PROJECTS/SOME ANIMATIONS/script.js
+++ 08-PROJECTS/SOME ANIMATIONS/script.js
@@ -21,3 +21,25 @@ button.addEventListener("click", () => {
     button.classList.remove("clicked");
   }, 200);
 });
+
+const bubbleBtn = document.getElementById("bubble");
+bubbleBtn.addEventListener("click", (event)=> {
+    // get button's position and size
+    const rect = bubbleBtn.getBoundingClientRect();
+    // click's position in the btn
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+  // create a span element in the HTML
+    const effect = document.createElement("span");
+// add the ripple to the effect
+    effect.classList.add("ripple");
+// now position the span
+    effect.style.left = `${x}px`;
+    effect.style.top = `${y}px`;
+// put the ripple in the btn
+  bubbleBtn.appendChild(effect);
+
+  effect.addEventListener("animationend", () => {
+    effect.remove();
+  });
+}); 
