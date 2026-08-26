b94576537dd63914e57e05420ac0209d4a1b76e4
bxluwatif3 <sammiemarvie03@gmail.com>
2026-08-26 21:18:03 +0100

    added a progress bar and reset to the timer

--- 08-PROJECTS/PULSE/CODE/JS/app.js
+++ 08-PROJECTS/PULSE/CODE/JS/app.js
@@ -1,7 +1,15 @@
 // DOM ELEMENTS
 const taskBtn = document.querySelector(".add-task");
+const taskInput = document.querySelector(".task-input");
+const taskInputArea = document.querySelector(".task-input-area");
+const saveTaskBtn = document.querySelector(".save-task");
+const taskList = document.querySelector(".task-list");
+const newTask = document.createElement("div");
 const focusBtn = document.querySelector(".start-focus");
+const resetBtn = document.querySelector(".reset-focus");
 const timer = document.querySelector(".timer");
+const focusStatus = document.querySelector(".focus-status");
+const progressFill = document.querySelector(".progress-fill")
 const totalTextCount = document.querySelector(".total-text-count");
 const completedTextCount = document.querySelector(".completed-text-count");
 const remainingTextCount = document.querySelector(".remaining-text-count");
@@ -18,8 +26,40 @@ const strong = document.querySelectorAll("strong");
 
 //event listener for the taskBtn
 taskBtn.addEventListener("click", () => {
-    taskBtn.textContent = "Task added!";
+    // taskBtn.textContent = "Task added!";
+    taskInputArea.style.opacity = 1;
     // return;
+})
+
+// add new tasks
+
+
+saveTaskBtn.addEventListener("click", () => {
+        if (taskInput.value === "") {
+        return;
+    }
+
+    const newTask = document.createElement("div");
+
+    newTask.classList.add("task");
+
+    newTask.innerHTML = `
+        <button class="checkbox"></button>
+
+        <div class="task-info">
+            <h3>${taskInput.value}</h3>
+            <p>Personal</p>
+        </div>
+
+        <span class="priority low">
+            Low
+        </span>
+    `;
+
+    taskList.appendChild(newTask);
+
+    taskInput.value = "";
+
 })
 
 // calculate tasks stats
@@ -65,41 +105,72 @@ updateStats();
 // timer
 
 
-let timeLeft = 25 * 60;
-let timeInterval;
+const totalTime = 25 * 60;
+let timeLeft = totalTime;
+let timerInterval;
 let isRunning = false;
-let isPaused = false;
+
+function displayTime (){
+        
+            const minutes = Math.floor(timeLeft / 60);
+            const seconds = timeLeft % 60;
+            const progress = ((totalTime - timeLeft)/ totalTime * 100);
+            progressFill.style.width = `${progress}%`;
+
+            timer.textContent =
+                `${minutes}:${seconds.toString().padStart(2, "0")}`;
+
+}
+
+displayTime();
 
 focusBtn.addEventListener("click", () => {
+// shorthand for if (isRunning === true) 
+// pause timer
+   if (isRunning) {
+        // pause timer
+        clearInterval(timerInterval);
 
-    if (isRunning) {
-        return;
+        isRunning = false;
+        focusBtn.textContent = "Resume focus session";
+        focusStatus.textContent = "Paused"
+    } else {
+
+        // START / RESUME
+        // resume timer when clicked
+
+        isRunning = true;
+
+        focusBtn.textContent = "Pause focus session";
+        focusStatus.textContent = "Running";
+        timerInterval = setInterval(() => {
+
+            timeLeft--;
+
+           displayTime();
+            if (timeLeft <= 0) {
+
+                clearInterval(timerInterval);
+
+                isRunning = false;
+
+                focusBtn.textContent = "Start focus session";
+            }
+
+        }, 1000);
+
     }
-
-    isRunning = true;
-    focusBtn.textContent = "Focus session running";
-
-    timerInterval = setInterval(() => {
-
-        timeLeft--;
-
-        const minutes = Math.floor(timeLeft / 60);
-        const seconds = timeLeft % 60;
-
-        timer.textContent =
-            `${minutes}:${seconds.toString().padStart(2, "0")}`;
-
-        if (timeLeft <= 0) {
-
-            clearInterval(timerInterval);
-
-            isRunning = false;
-
-            focusBtn.textContent = "Start focus session";
-        }
-
-    }, 1000);
-
 
 
 });
+
+//reset btn 
+resetBtn.addEventListener("click", () =>{
+    clearInterval(timerInterval);
+    timeLeft = 25 * 60;
+    displayTime();
+
+    isRunning = false;
+    focusBtn.textContent = "Start focus session";
+    focusStatus.textContent = "Ready";
+})
